24a88be11d5e2cecaf870d240b597aafc04f32cb
Arun <Arun@DESKTOP-B8JJQ9J>
2021-01-29 15:56:30 -0600

    Sample class pushing to GIT

---
+++ Selenium_Git/src/TestCases/coreJava.java
@@ -0,0 +1,12 @@
+package TestCases;
+
+public class coreJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
